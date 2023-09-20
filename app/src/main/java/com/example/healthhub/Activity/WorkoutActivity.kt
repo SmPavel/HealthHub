b575d127d5dea6af0578f98f8a65620e92c517c4
SmPavel <76187376+SmPavel@users.noreply.github.com>
2023-09-20 13:25:08 +0300

    ver 1.1

--- app/src/main/java/com/example/healthhub/Activity/WorkoutActivity.kt
+++ app/src/main/java/com/example/healthhub/Activity/WorkoutActivity.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.healthhub.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -31,6 +32,9 @@ class WorkoutActivity : AppCompatActivity() {
     private lateinit var adapter: WorkoutPlanAdapter
     private lateinit var db: FirebaseFirestore
     private val data = mutableListOf<WorkoutData>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
@@ -47,40 +51,48 @@ class WorkoutActivity : AppCompatActivity() {
 
         db = FirebaseFirestore.getInstance()
         adapter = WorkoutPlanAdapter(this, data)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
         fetchData()
 
         btnAdd.setOnClickListener {
-            // Получение текста из EditText
             val workName = etWorkName.text.toString().trim()
             val workCountText = etWorkCount.text.toString().trim()
 
-            // Проверка, что значения не являются пустыми строками
             if (workName.isNotEmpty() && workCountText.isNotEmpty()) {
                 val workCount = workCountText.toLong()
                 val competed = false
 
-                // Создание нового документа в коллекции "workout"
-                db.collection("workout")
-                    .add(
-                        mapOf(
-                            "name" to workName,
-                            "count" to workCount,
-                            "completed" to competed
-                        )
-                    )
-                    .addOnSuccessListener { documentReference ->
-                        fetchData()
+                val workoutData = hashMapOf(
+                    "userId" to userId,
+                    "name" to workName,
+                    "count" to workCount,
+                    "completed" to competed
+                )
 
-                        // Очистка содержимого EditText
+                db.collection("workout")
+                    .add(workoutData)
+                    .addOnSuccessListener { documentReference ->
+                        val documentId = documentReference.id
+                        val newWorkData = WorkoutData(workName, workCount, competed, documentId)
+                        data.add(newWorkData)
+                        adapter.notifyDataSetChanged()
+
+                        // Scroll the RecyclerView to the new item
+                        recyclerView.scrollToPosition(adapter.itemCount - 1)
+
+                        // Clear input fields
                         etWorkName.setText("")
                         etWorkCount.setText("")
                     }
+                    .addOnFailureListener { exception ->
+                        // Handle the failure case if necessary
+                        Log.e(TAG, "Error adding workout data: $exception")
+                    }
             }
         }
-
 
         btnBack.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
@@ -91,8 +103,12 @@ class WorkoutActivity : AppCompatActivity() {
                   deleteCompletedExercises()
             }
     }
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser
+    val userId = currentUser!!.uid
     private fun fetchData() {
         db.collection("workout")
+            .whereEqualTo("userId", userId)
             .get()
             .addOnSuccessListener { querySnapshot ->
                 val snapshotList = querySnapshot.documents
@@ -109,6 +125,8 @@ class WorkoutActivity : AppCompatActivity() {
                     }
                 }
                 adapter.notifyDataSetChanged()
+
+                recyclerView.scrollToPosition(adapter.itemCount - 1)
             }
     }
 
